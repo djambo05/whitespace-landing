98eb2ddf216204639f095ce74076df7714f58a2b
Djamalutin <fisher.gerryy@mail.ru>
2023-05-16 23:33:50 +0300

    feature: css AppBar 1400px

--- src/Components/Layout/AppBar.jsx
+++ src/Components/Layout/AppBar.jsx
@@ -1,32 +1,56 @@
 import "./style.scss";
 export const AppBar = () => {
+  const menuData = [
+    {
+      id: 1,
+      title: "Products",
+      img: "arrowDown.png",
+      alt: "arrow",
+    },
+    {
+      id: 2,
+      title: "Solutions",
+      img: "arrowDown.png",
+      alt: "arrow",
+    },
+    {
+      id: 3,
+      title: "Resources",
+      img: "arrowDown.png",
+      alt: "arrow",
+    },
+    {
+      id: 4,
+      title: "Pricing",
+      img: "arrowDown.png",
+      alt: "arrow",
+    },
+  ];
   return (
     <div className="container">
-      <div>
-        <img srcSet="./img/logo.png" alt="logo"></img>
+      <div className="left-div">
+        <div>
+          <img src="./img/logo.png" alt="logo" />
+        </div>
         <span className="left-span">whitespace</span>
       </div>
-      <div className="center-div">
-        <div>
-          <span>Products</span>
-          <img srcSet="./img/arrowDown.png" alt="arrow"></img>
+      <div className="compress-div">
+        <div className="center-div">
+          {menuData.map((obj) => {
+            return (
+              <div className="item-center-div" key={obj.id}>
+                <span className="item-span">{obj.title}</span>
+                <div>
+                  <img src={`./img/${obj.img}`} alt={obj.alt}></img>
+                </div>
+              </div>
+            );
+          })}
         </div>
-        <div>
-          <span>Solutions</span>
-          <img srcSet="./img/arrowDown.png" alt="arrow"></img>
+        <div className="right-div">
+          <button className="login-btn">Login</button>
+          <button className="try-btn">Try Whitespace free</button>
         </div>
-        <div>
-          <span>Recourses</span>
-          <img srcSet="./img/arrowDown.png" alt="arrow"></img>
-        </div>
-        <div>
-          <span>Pricing</span>
-          <img srcSet="./img/arrowDown.png" alt="arrow"></img>
-        </div>
-      </div>
-      <div>
-        <button></button>
-        <button></button>
       </div>
     </div>
   );
